b80d992ebe9225ca55e3df81a8bd1fa797fcb0c7
Jorge Luis Hernández <shadday007@gmail.com>
2021-09-18 12:21:30 -0400

    add some key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,5 @@
- 
+vim.g.mapleader = "<Space>"
+vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", {noremap = true})
+vim.api.nvim_set_keymap("x", "<Space>", "<NOP>", {noremap = true})
+
+vim.api.nvim_set_keymap( "n", "<Space>dd", ":Lexplore<CR>", { noremap = true , silent = true })
